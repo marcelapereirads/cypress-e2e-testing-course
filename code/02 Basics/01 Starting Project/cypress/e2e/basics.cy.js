ac9adfec1c239dd4879746c1103637eb74766a1e
Marcela Silva <Marcela.Silva@ctw.bmwgroup.com>
2023-04-16 16:50:07 +0100

    First e2e test

---
+++ code/02 Basics/01 Starting Project/cypress/e2e/basics.cy.js
@@ -0,0 +1,8 @@
+/// <reference types="Cypress" />
+
+describe('tasks page', () => {
+  it('should render the main page', () => {
+    cy.visit('http://127.0.0.1:5173/');
+    cy.get('');
+  });
+});
