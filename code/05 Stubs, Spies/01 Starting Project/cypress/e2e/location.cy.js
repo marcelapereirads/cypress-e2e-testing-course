da27908a30e1dc40d3259d290c72a3c4a5c51fa8
Marcela Silva <Marcela.Silva@ctw.bmwgroup.com>
2023-07-10 19:03:15 +0100

    Mock function using callsFake

--- code/05 Stubs, Spies/01 Starting Project/cypress/e2e/location.cy.js
+++ code/05 Stubs, Spies/01 Starting Project/cypress/e2e/location.cy.js
@@ -4,9 +4,23 @@ describe('share location', () => {
   it('should fetch the user location', () => {
     // visit yield the window object
     cy.visit('/').then((win) => {
-      cy.stub(win.navigator.geolocation, 'getCurrentPosition').as('getUserPosition');
+      cy.stub(win.navigator.geolocation, 'getCurrentPosition')
+        .as('getUserPosition')
+        .callsFake((_cb) => {
+          // setTimeout is needed to validate the button disabled
+          setTimeout(() => {
+            _cb({
+              coords: {
+                latitude: 15.53,
+                longitude: 39.48
+              }
+            });
+          }, 100);
+        })
     });
     cy.get('[data-cy="get-loc-btn"]').click();
     cy.get('@getUserPosition').should('have.been.called');
+    cy.get('[data-cy="get-loc-btn"]').should('be.disabled');
+    cy.get('[data-cy="actions"]').contains('Location fetched');
   });
 });
