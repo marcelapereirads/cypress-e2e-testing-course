d38d92adc3abd00617c65f7a63b526473b546ad6
Marcela Silva <Marcela.Silva@ctw.bmwgroup.com>
2023-07-13 17:57:07 +0100

    Location e2e tests

--- code/05 Stubs, Spies/01 Starting Project/cypress/e2e/location.cy.js
+++ code/05 Stubs, Spies/01 Starting Project/cypress/e2e/location.cy.js
@@ -1,26 +1,60 @@
 /// <reference types="cypress" />
 
 describe('share location', () => {
-  it('should fetch the user location', () => {
+  beforeEach(() => {
+    // need to include clock to use tick function to skip time
+    cy.clock();
+
+    // convert JSON in Javascript object
+    cy.fixture('user-location.json').as('userLocation');
+
     // visit yield the window object
     cy.visit('/').then((win) => {
-      cy.stub(win.navigator.geolocation, 'getCurrentPosition')
+      cy.get('@userLocation').then((currentLocation) => {
+        cy.stub(win.navigator.geolocation, 'getCurrentPosition')
         .as('getUserPosition')
         .callsFake((_cb) => {
           // setTimeout is needed to validate the button disabled
           setTimeout(() => {
-            _cb({
-              coords: {
-                latitude: 15.53,
-                longitude: 39.48
-              }
-            });
+            _cb(currentLocation);
           }, 100);
-        })
+        });
+      });
+
+      cy.stub(win.navigator.clipboard, 'writeText')
+        .as('writeTextToClipboard')
+        .resolves();
+
+      cy.spy(win.localStorage, 'setItem').as('setLocalStorage');
+      cy.spy(win.localStorage, 'getItem').as('getLocalStorage');
     });
+  });
+
+  it('should fetch the user location', () => {
     cy.get('[data-cy="get-loc-btn"]').click();
     cy.get('@getUserPosition').should('have.been.called');
     cy.get('[data-cy="get-loc-btn"]').should('be.disabled');
     cy.get('[data-cy="actions"]').contains('Location fetched');
+  });
+
+  it('should share the location URL', () => {
+    cy.get('[data-cy="name-input"]').type('Marcela Silva');
+    cy.get('[data-cy="get-loc-btn"]').click();
+    cy.get('[data-cy="share-loc-btn"]').click();
+    cy.get('@userLocation').then((currentLocation) => {
+      const { latitude, longitude } = currentLocation.coords;
+      const locationUrlRegex = new RegExp(`${latitude}.*${longitude}.*${encodeURI('Marcela Silva')}`);
+
+      cy.get('@writeTextToClipboard')
+        .should('have.been.calledWithMatch', locationUrlRegex);
+      cy.get('@setLocalStorage').should('always.have.been.calledWithMatch', 'Marcela Silva', locationUrlRegex);
+    });
+
+    cy.get('[data-cy="share-loc-btn"]').click();
+    cy.get('@getLocalStorage').should('have.been.called');
+    cy.get('[data-cy="info-message"]').should('be.visible');
+
+    cy.tick(2000);
+    cy.get('[data-cy="info-message"]').should('not.be.visible');
   });
 });
